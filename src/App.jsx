d865f0334269f2cd195fbdb163bdef258e461d04
kouss <koceila.habarek94@gmail.com>
2024-05-22 12:31:14 +0200

    add array

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 // src/App.jsx
 import React from 'react';
 import Shopping from './component';
-import Card from './card';
+// import Card from './card';
 
 function App() {
+  const products = [
+    { name: 'Oeufs', quantity: 6},
+    { name: 'Avocat', quantity: 3},
+    { name: 'Banane', quantity: 4},
+  ]
   return (
-    <div className="App">
-      <Shopping title="Liste des courses" />
-      <Card title="Pooker" />
-
-    </div>
-  );
-}
-
-export default App;
+    <Shopping
+      title="Ma liste de courses !"
+      products={products} />
+    );
+    // <div className="App">
+    //   <Shopping title="Liste des courses" />
+    //   <Card title="Pooker" />
+  
+    // </div>
+  }
+  
+  export default App;
+  
--- src/component.jsx
+++ src/component.jsx
@@ -1,15 +1,19 @@
-import Product from "./product.jsx"
-function Shopping( props ) {
-    return(
-        <div>
-            <h1>{ props.title}</h1>
-            <ul>
-                <li>
-                    <Product name="Oeufs" quantity="6"/>
-                </li>
-            </ul>
-        </div>
-    )
+import React from 'react';
+import Product from './product.jsx';
+
+function Shopping({ title, products }) {
+  return (
+    <div className="shopping">
+      <h1>{title}</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            <Product name={product.name} quantity={product.quantity} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Shopping
+export default Shopping;
